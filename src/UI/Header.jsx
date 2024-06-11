868c87c9542ba22853dd86e91e562ef97a4cd88b
Rajan Singh <82579513+Rajan1902@users.noreply.github.com>
2024-06-11 15:49:30 +0530

    Update Header.jsx

--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -5,7 +5,7 @@ import UserName from "../features/user/UserName"
 const Header = () => {
   return (
     <header className="bg-yellow-400 uppercase border-b-2 border-stone-300 px-4 py-3 sm:px-6 flex items-center justify-between">
-        <Link to='/' className="tracking-widest">React Pizza Co.</Link>
+        <Link to='/' className="tracking-widest">Rajan's Pizza</Link>
         <SearchOrder></SearchOrder>
         <UserName></UserName>
     </header>
